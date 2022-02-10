3fb51e9820e4a3ebc2e2dc0ecac6bd90faef1cf7
Raul Garcia <punkvomit21@gmail.com>
2022-02-10 03:19:48 -0500

    change display property and music notes for nextTwo

--- main.js
+++ main.js
@@ -41,11 +41,20 @@ startOver.hidden= true;
 nextOne.addEventListener('click', () => {
   nextTwo.hidden = false;
   nextOne.hidden = true;
+  document.getElementById('letter-note-five').innerHTML = 'D';
+  document.getElementById('letter-note-six').innerHTML = 'C';
 });
 
 nextTwo.addEventListener('click', () => {
   nextThree.hidden = false;
   nextTwo.hidden = true;
+  document.getElementById('word-five').innerHTML = 'DEAR';
+  document.getElementById('word-six').innerHTML = 'FRI-';
+  lastLyric.style.display = 'inline-block';
+  document.getElementById('letter-note-three').innerHTML = 'G';
+  document.getElementById('letter-note-four').innerHTML = 'E';
+  document.getElementById('letter-note-five').innerHTML = 'C';
+  document.getElementById('letter-note-six').innerHTML = 'B';
 });
 
 nextThree.addEventListener('click', () => {
